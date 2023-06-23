ea5f1f1e270262d4d07bd7ed55514833adfbd080
Jesus Borjas <jesusbor006@gmail.com>
2023-06-23 18:36:54 -0500

    Add all backgrounds

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,15 @@ import "./App.css";
 function App() {
   return (
     <main className="main">
-        <Navbar />
-      <div className="background-1">
-      </div>
-      <div className="background-2">
-        {/* <Navbar /> */}
-      </div>
+      <Navbar />
+      <div className="background-1"></div>
+      <div className="background-2"></div>
+      <div className="background-3"></div>
+      <div className="background-4"></div>
+      <div className="background-5"></div>
+      <div className="background-6"></div>
+      <div className="background-7"></div>
+      <div className="background-8"></div>
     </main>
   );
 }
